5e81722eab99032b07f39166750b7f7cda48a8b1
tigertailzlc <tigertailzlc@gmail.com>
2016-08-04 20:56:35 +0800

    Beginning mashup edits for deployment

--- sausage/sausage.py
+++ sausage/sausage.py
@@ -111,3 +111,24 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('show_entries'))
 
+
+
+# This is where I have no idea what I'm doing, but I'm trying to 
+# deploy to Heroku. Have read Procfile documentation but understanding 
+# of Flask and Heroku in tandem still very hazy; therefore will simply 
+# make my lil dyno run python sausage.py and see what happens 
+
+# But for that to happen I have to have the main function 
+# so here goes 
+
+if __name__ == "__main__": 
+    #I have to initialize the database first, right? 
+    #But instead of using the CLI I'll just run the fn directly: 
+    init_db()
+    print 'Initialized the database. Whooop de dooo dooo.'
+    #let's try screwing with this. Having read Python 2.7 os documentation, 
+    #replacing os.environ.get with os.getenv. 
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+# Let's see how that goes... 
